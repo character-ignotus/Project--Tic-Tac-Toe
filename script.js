b30270f005488e04671e24f74bf2ee60c0adc5c1
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-11-04 18:42:13 +0200

    Create a factory function for players
    Each player's input is stored inside of an array created from the FF.

--- script.js
+++ script.js
@@ -15,20 +15,36 @@ const gameboard = (() => {
 
         if(round % 2 == 0) {
             gameboardArray[row][column] = 'X';
+            player1.logInput(row, column);
             e.target.textContent = gameboardArray[row][column];
             round += 1;
         } else {
             gameboardArray[row][column] = 'O';
+            player2.logInput(row, column);
             e.target.textContent = gameboardArray[row][column];
             round += 1;
         }
-
-        console.log(row);
-        console.log(column);
+    
         console.log(gameboardArray);
+        console.log(player1.array);
+        console.log(player2.array);
     };
 
     return {bindEvents};
 })();
 
 gameboard.bindEvents();
+
+
+const Player = () => {
+    let array = [[], [], []];
+
+    const logInput = (row, column) => {
+        array[row][column] = 1;
+    };
+
+    return {array, logInput};
+};
+
+const player1 = Player();
+const player2 = Player();
