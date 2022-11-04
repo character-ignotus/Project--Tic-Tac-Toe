3d582dbc46e2a2a76fb63dbcd46b706efa942094
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2022-11-04 18:26:24 +0200

    Link gameboard cells with gameboard object

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const gameboard = (() => {
     let gameboardArray = [[], [], []];
     let round = 0;
+    const cells = Array.from(document.querySelectorAll('.cell'));
+
+    const bindEvents = () => {
+        cells.forEach(cell => {
+            cell.addEventListener('click', addInput);
+        });
+    };
+
+    function addInput(e) {
+        let row = e.target.getAttribute('data-row');
+        let column = e.target.getAttribute('data-column');
+
+        console.log(row);
+        console.log(column);
+    };
+
+    return {bindEvents};
 })();
+
+gameboard.bindEvents();
